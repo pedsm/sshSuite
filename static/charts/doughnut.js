ed158f267e75e1c0f9c3da260d9641a5f1c6217b
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-03-11 18:06:39 +0000

    added op chart

---
+++ static/charts/doughnut.js
@@ -0,0 +1,14 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Used Memory", "Available Memory"],
+        datasets: [{
+            backgroundColor: [
+                "#3498db",
+                "#95a5a6",
+            ],
+            data: [2000, 3000]
+        }]
+    }
+});
