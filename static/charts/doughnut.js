5bacf9366f684b3df5991094b06abc17887688b8
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-03-11 19:14:12 +0000

    finished OP chart

--- static/charts/doughnut.js
+++ static/charts/doughnut.js
@@ -1,14 +1,16 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["Used Memory", "Available Memory"],
-        datasets: [{
-            backgroundColor: [
-                "#3498db",
-                "#95a5a6",
-            ],
-            data: [2000, 3000]
-        }]
-    }
-});
+function makeMemChart(array){
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Used Memory", "Available Memory"],
+            datasets: [{
+                backgroundColor: [
+                    "#3498db",
+                    "#95a5a6",
+                ],
+                data: array
+            }]
+        }
+    });
+}
