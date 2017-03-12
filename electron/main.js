7ef3235637f9eaaa47c7efdc3fa3d697b11836bc
Pedro Mendonca <peumendonca@gmail.com>
2017-03-12 03:51:54 +0000

    First graph

--- electron/main.js
+++ electron/main.js
@@ -1,16 +1,16 @@
 // You can also require other files to run in this process
 //require('./renderer.js')
 const gritty = require('gritty'),
-      http = require('http'),
-      express = require('express'),
-      io = require('socket.io'),
-      exec = require('child_process').exec,
-      termApp = express(),
-      server = http.createServer(termApp),
-      socket = io.listen(server),
-      port = 3000,
-      ip = '0.0.0.0',
-      config = require('./config.json')
+    http = require('http'),
+    express = require('express'),
+    io = require('socket.io'),
+    exec = require('child_process').exec,
+    termApp = express(),
+    server = http.createServer(termApp),
+    socket = io.listen(server),
+    port = 3000,
+    ip = '0.0.0.0',
+    config = require('./config.json')
 var toggleGraphs = false;
 
 termApp.use(gritty())
@@ -20,17 +20,51 @@ termApp.use(express.static(__dirname));
 gritty.listen(socket);
 server.listen(port, ip);
 
+setInterval(()=>{sshExec('cat /proc/meminfo',cutMem)},10000)
+setInterval(()=>{sshExec('ps -eo pcpu',cpuAvg)},1000)
+
+memFree = 1
+memUsed = 1
+function cutMem(input)
+{
+    var res = input.split("\n", 3)
+    res.map((ob)=>parseInt(ob))
+    newRes = []
+    res.forEach((line,index)=>
+        {
+            newRes[index] = line.split(" ")
+        })
+    var memTotal = newRes[0][9];
+    memFree = newRes[1][11];
+    memUsed = memTotal - memFree;
+    console.log("Memory used is " + memUsed);
+    console.log("Memory free is " + memFree);
+}
+
+function cpuAvg(input)
+{
+    var counter = 0;
+    var res = input.split("\n")
+    res[0] = 0;
+
+    for (var x = 0; x < res.length-1; x++) {
+        res[x] = parseFloat(res[x]);
+        counter = counter + res[x];
+    }
+    console.log(counter)
+}
+
 function toggle()
 {
     if(toggleGraphs)
     {
-        document.getElementById("footer").style.bottom = "-26vh"
+        document.getElementById("footer").style.bottom = "-230px"
         // document.getElementById("iframe").style.height = "74vh"
         document.getElementById("arrow").style.transform = "rotate(0deg)"
     }
     else
     {
-        document.getElementById("footer").style.bottom = "0vh"
+        document.getElementById("footer").style.bottom = "0px"
         // document.getElementById("iframe").style.height = "96vh"
         document.getElementById("arrow").style.transform = "rotate(180deg)"
     }
@@ -51,12 +85,10 @@ function getPath()
 {
     if(document.getElementById('iframe').contentWindow.emulator.terminal.title.split(":")[1] == undefined)
     {
-        console.log('recurse')
         return getPath()
     }
     else
     {
-        console.log( document.getElementById('iframe').contentWindow.emulator.terminal.title.split(":")[1] )
         return document.getElementById('iframe').contentWindow.emulator.terminal.title.split(":")[1]
     }
 }
@@ -94,7 +126,7 @@ function drawLs(path)
                     html += '<div onclick="changeDir(\''+res.split("\n")[i].split(" ")[res.split("\n")[i].split(" ").length -1]+'\',\'edit\')" class="item">';
                     html += '<i class="material-icons">insert_drive_file</i>';
                 }
-                console.log(res.split("\n")[i].split(" ")[res.split("\n")[i].split(" ").length -1])
+                // console.log(res.split("\n")[i].split(" ")[res.split("\n")[i].split(" ").length -1])
                 html += '<span>'+ res.split("\n")[i].split(" ")[res.split("\n")[i].split(" ").length -1] + '</span>';
                 html += '</div>'
             }
@@ -105,6 +137,49 @@ function drawLs(path)
 function startLs()
 {
     drawLs('')
+}
+function makeMemChart(array){
+    var ctx = document.getElementById("doughnut").getContext('2d');
+    var doughnut = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Used Memory", "Available Memory"],
+            datasets: [{
+                backgroundColor: [
+                    "#3498db",
+                    "#cc0000",
+                ],
+                data: array
+            }]
+        }
+    });
+    return doughnut;
+}
+
+function makeCpuChart(array){
+    var ctx = document.getElementById("lineGraph").getContext('2d');
+    var lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {datasets:[{data:array}]}
+    });
+    return lineChart;
+}
+function plsWork(){ 
+    myArray = [50,60,10,90,4]
+    var ctx = document.getElementById("lineGraph").getContext('2d');
+    var line = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [1,2,3,4,5],
+            datasets: [{
+                backgroundColor: [
+                    "#3498db",
+                ],
+                data: myArray
+            }]
+        }
+    });
+    return line;
 }
 startLs();
 
